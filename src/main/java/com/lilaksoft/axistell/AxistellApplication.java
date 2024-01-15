68f79fef0e20f336204177f889650d2bc7a35744
Karar Tuncay (UniCredit Services - AT) <N992726@hd00.UNICREDITGROUP.EU>
2024-01-15 16:29:24 +0100

    initial commit

---
+++ src/main/java/com/lilaksoft/axistell/AxistellApplication.java
@@ -0,0 +1,13 @@
+package com.lilaksoft.axistell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxistellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxistellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lilaksoft/axistell/AxistellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lilaksoft.axistell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxistellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
